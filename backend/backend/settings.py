4ac975dec6bc4cdc3652d0e829130a3af25ec5c8
Coezette Sibanda <cs3353@nyu.edu>
2026-03-12 21:28:27 -0400

    debuggin cloudinary

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -15,9 +15,8 @@ from datetime import timedelta
 from dotenv import load_dotenv
 import dj_database_url
 import os
-import cloudinary
  
-load_dotenv()
+load_dotenv(override=False)
 
 ENVIRONMENT= os.environ.get('ENVIRONMENT', default='production')
 
@@ -206,9 +205,9 @@ SIMPLE_JWT = {
 
 ACCOUNT_USERNAME_BLACKLIST = ['admin', 'accounts', 'profile', 'recipe', 'maneja']
 
-# TEMPORARY DEBUG - remove after fixing
-from django.core.files.storage import default_storage
+# TEMPORARY DEBUG - to be removed after fixing
+
 import sys
-print('=== STORAGE BACKEND ===', default_storage.__class__, file=sys.stderr)
-print('=== CLOUDINARY CLOUD NAME ===', os.environ.get('CLOUDINARY_CLOUD_NAME'), file=sys.stderr)
-print('=== ENVIRONMENT ===', os.environ.get('ENVIRONMENT'), file=sys.stderr)
+print('=== CLOUDINARY_CLOUD_NAME ===', os.environ.get('CLOUDINARY_CLOUD_NAME'), file=sys.stderr)
+print('=== STORAGE BACKEND ===', DEFAULT_FILE_STORAGE, file=sys.stderr)
+print('=== ENVIRONMENT ===', ENVIRONMENT, file=sys.stderr)
